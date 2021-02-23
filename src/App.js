2ff671fafee8bc06def3efbb36ff2317f42303f6
yectofan <yectofan2@hotmail.co.uk>
2021-02-23 17:19:25 +0000

    added CNAME file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Joe Tomlinson - Project Showcase</h1>
-      <h3>Website in development</h3>
+      <h3>Website currently in development</h3>
     </div>
   );
 }
