10a481d61fa52e395cad6f4729ca8ed53656517b
yectofan <yectofan2@hotmail.co.uk>
2021-02-24 18:07:14 +0000

    removed 404 and added redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+import {Switch, Route, Redirect, BrowserRouter as Router} from 'react-router-dom';
 import Home from './Pages/Home';
 import CV from './Pages/CV';
 
@@ -10,8 +10,9 @@ function App() {
       <Router className="Page" basename={`/${process.env.PUBLIC_URL}`}>
         <Navbar/>
         <Switch>
+          <Route path="/" exact component={Home}/>
           <Route path="/CV" exact component={CV}/>
-          <Route path="/" component={Home}/>
+          <Redirect to="/"/>
         </Switch>
       </Router>
     </div>
