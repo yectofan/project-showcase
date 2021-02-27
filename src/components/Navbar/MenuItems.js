137a275057029662b778f2f95e40c0a71074ca2b
yectofan <yectofan2@hotmail.co.uk>
2021-02-27 23:51:27 +0000

    added projects to menu

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -13,17 +13,52 @@ export const MenuItems = [
 
 export const DropdownItems = [
     {
-        title: 'Project 1',
+        title: 'Hole in the Wall',
         url: '#',
         cName: 'dropdown-links'
     },
     {
-        title: 'Project 2',
+        title: 'VR firepit',
         url: '#',
         cName: 'dropdown-links'
     },
     {
-        title: 'Project 3',
+        title: 'VR Data Visualisation',
+        url: '#',
+        cName: 'dropdown-links'
+    },
+    {
+        title: 'Ball physics',
+        url: '#',
+        cName: 'dropdown-links'
+    },
+    {
+        title: 'Banking App',
+        url: '#',
+        cName: 'dropdown-links'
+    },
+    {
+        title: 'Macabre Maze',
+        url: '#',
+        cName: 'dropdown-links'
+    },
+    {
+        title: '3D Renderer',
+        url: '#',
+        cName: 'dropdown-links'
+    },
+    {
+        title: '3D Modelling',
+        url: '#',
+        cName: 'dropdown-links'
+    },
+    {
+        title: 'Stack',
+        url: '#',
+        cName: 'dropdown-links'
+    },
+    {
+        title: 'Rooms',
         url: '#',
         cName: 'dropdown-links'
     },
